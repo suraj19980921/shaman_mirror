916ae7094785ab18c5dad80caeaa6aed89d3bdaa
Suraj Kumar <sk9966332@gmail.com>
2025-06-23 22:20:17 +0530

    all hosts allowed

--- shaman_mirror/settings.py
+++ shaman_mirror/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wl3xrdxj5(^8a=c*@)*-p14f7rq)s6^0mn)(!+^nqdjyaavsks
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
